fb7b260ec77599bf2b1355ad1b069995bbac5b8b
wangchao.wang <wangchao.wang@qunar.com>
2016-04-22 23:52:28 +0800

    index.html

--- src/main/java/com/star/blog/controller/BaseController.java
+++ src/main/java/com/star/blog/controller/BaseController.java
@@ -1,10 +1,20 @@
 package com.star.blog.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 /**
- * @author <a href="mailto:wangchao.wang@qunar.com">wangchao.wang</a>
+ * @author <a href="mailto:wangchao.star@gmail.com">wangchao</a>
  * @date 2016-04-22 18:17:00
  */
 @Controller
-@RequestMapping(value = "/qb/platfui/adapter/ctrip")
+@RequestMapping(value = "/star/blog")
 public class BaseController {
+
+    @RequestMapping("/home")
+    @ResponseBody
+    public Object index() {
+        return "启动成功";
+    }
 }
