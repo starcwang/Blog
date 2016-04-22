740fdd659deb76f877581bc0fbcb2b8f4f790627
wangchao.wang <wangchao.wang@qunar.com>
2016-04-22 18:57:33 +0800

    index

---
+++ src/main/java/com/star/blog/controller/BaseController.java
@@ -0,0 +1,10 @@
+package com.star.blog.controller;
+
+/**
+ * @author <a href="mailto:wangchao.wang@qunar.com">wangchao.wang</a>
+ * @date 2016-04-22 18:17:00
+ */
+@Controller
+@RequestMapping(value = "/qb/platfui/adapter/ctrip")
+public class BaseController {
+}
